67e0f8f8ef8fc191929856d44e9ca57fb4d73717
David Solis <github@soliskit.com>
2020-04-04 18:19:57 -0700

    add downloading protocol and conform downloadClient to URLSessionDelegate

--- PitchShifter/DownloadClient.swift
+++ PitchShifter/DownloadClient.swift
@@ -8,6 +8,117 @@
 
 import Foundation
 
+class DownloadClient: NSObject, Downloading {
+    
+    // MARK: - Properties
+    var delegate: DownloadingDelegate?
+    var completionHandler: ((Error?) -> Void)?
+    var progressHandler: ((Data, Double) -> Void)?
+    var progress: Double = 0
+    
+    var state: DownloadingState = .notStarted {
+        didSet {
+            delegate?.download(self, changedState: state)
+        }
+    }
+    
+    var url: URL? {
+        didSet {
+            if state == .started {
+                stop()
+            }
+            if let url = url {
+                progress = 0.0
+                state = .notStarted
+                totalBytesCount = 0
+                totalBytesReceived = 0
+                task = session.dataTask(with: url)
+            } else {
+                task = nil
+            }
+        }
+    }
+
+    /// The `URLSession` currently being used as the HTTP/HTTPS implementation for the downloader.
+    private lazy var session: URLSession = {
+        return URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+    }()
+
+    /// A `URLSessionDataTask` representing the data operation for the current `URL`.
+    private var task: URLSessionDataTask?
+
+    /// A `Int` representing the total amount of bytes received
+    var totalBytesReceived: Int = 0
+
+    /// A `Int` representing the total amount of bytes for the entire file
+    var totalBytesCount: Int = 0
+    
+    // MARK: - Downloading Protocol
+    
+    func start() {
+        guard let task = task else {
+            fatalError("Task does not exist")
+        }
+        
+        switch state {
+            case .completed, .started:
+                return
+            default:
+                state = .started
+                task.resume()
+        }
+    }
+    
+    func pause() {
+        guard let task = task else {
+            fatalError("Task does not exist")
+        }
+        guard state == .started else {
+            return
+        }
+        
+        state = .paused
+        task.suspend()
+    }
+    
+    func stop() {
+        guard let task = task else {
+            fatalError("Task does not exist")
+        }
+        guard state == .started else {
+            return
+        }
+        
+        state = .stopped
+        task.cancel()
+    }
+}
+
+extension DownloadClient: URLSessionDelegate {
+    func urlSession(_ session: URLSession,
+                    dataTask: URLSessionDataTask,
+                    didReceive response: URLResponse,
+                    completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        totalBytesCount = Int(response.expectedContentLength)
+        completionHandler(.allow)
+    }
+
+    func urlSession(_ session: URLSession,
+                    dataTask: URLSessionDataTask,
+                    didReceive data: Data) {
+        totalBytesReceived += data.count
+        progress = Double(totalBytesReceived) / Double(totalBytesCount)
+        delegate?.download(self, didReceiveData: data, progress: progress)
+        progressHandler?(data, progress)
+    }
+
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        state = .completed
+        delegate?.download(self, completedWithError: error)
+        completionHandler?(error)
+    }
+}
+
 protocol Downloading: class {
     // MARK: - Properties
     
@@ -15,7 +126,7 @@ protocol Downloading: class {
     var delegate: DownloadingDelegate? { get set }
     
     /// The current progress of the downloader. Ranges from 0.0 - 1.0, default is 0.0
-    var progress: Float { get }
+    var progress: Double { get }
     
     /// The current state of the downloader. See `DownloadingState` for the different prossible states
     var state: DownloadingState { get }
@@ -38,7 +149,7 @@ protocol Downloading: class {
 protocol DownloadingDelegate: class {
     func download(_ download: Downloading, changedState state: DownloadingState)
     func download(_ download: Downloading, completedWithError error: Error?)
-    func download(_ download: Downloading, didReceiveData data: Data, progress: Float)
+    func download(_ download: Downloading, didReceiveData data: Data, progress: Double)
 }
 
 enum DownloadingState: String {
